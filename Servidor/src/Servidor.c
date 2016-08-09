95408e1b7d47759aca4d4f250a11b4cbc2b6311b
marcelocejas <cejas.marcelo@gmail.com>
2016-08-09 18:20:29 -0300

    se

--- Servidor/src/Servidor.c
+++ Servidor/src/Servidor.c
@@ -10,10 +10,18 @@
 
 #include "Servidor.h"
 
-t_config_server ServerConfig;
-
+t_config_server* ServerConfig;
 
 int main(void) {
-cargarConfiguracion("SERVER.cfg",ServerConfig);
-return EXIT_SUCCESS;
+	char* ConfigPath = "SERVER.cfg";
+	cargarConfiguracion(ConfigPath, ServerConfig);
+	int socketServer = IniciarSocketServidor(ServerConfig->puerto);
+	if(socketServer == -1){
+		printf("Error creando socket servidor");
+	}
+	if (AceptarConexionCliente(socketServer) == -1) {
+		printf("Error aceptanco conexion.");
+	}
+	finalizarConfig();
+	return EXIT_SUCCESS;
 }
